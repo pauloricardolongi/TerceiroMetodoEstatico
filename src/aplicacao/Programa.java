4cf41c7636eebc721280bf1a497d5bc62d2d4b01
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-06 04:03:18 -0300

    criado função no programa

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,36 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static final double PI = 3.14159;
+	
+	public static void main(String[] args) {
+		/*método estático: aplicação no próprio programa*/		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Entre com o valor do raio: ");
+		double raio = sc.nextDouble();
+		
+		double c = circunferencia(raio);
+		double v = volume(raio);
+		
+		System.out.printf("Circunferencia: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n ", v);
+		System.out.printf("PI valor: %.2f%n", PI);
+		
+		sc.close();;
+
+	}
+	  public static double circunferencia(double raio) {
+		  return 2.0 * PI * raio;
+	  }
+	  public static double volume(double raio) {
+		  return 4.0 * PI * raio * raio * raio / 3.0;
+	  }
+
+}
